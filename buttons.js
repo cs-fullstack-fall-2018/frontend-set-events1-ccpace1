31c8af7b5628eafd857b747d3e1a7f9fddc38098
Christopher Pace <Christopher_pace@ymail.com>
2018-08-15 14:48:19 -0500

    1&2

---
+++ buttons.js
@@ -0,0 +1,22 @@
+var theButton = document.getElementById("button1");
+var total1 = document.getElementById("player1Score");
+var buttonThe = document.getElementById("button2");
+var total2 = document.getElementById("player2Score");
+var resetter = document.getElementById("buttonReset");
+
+theButton.addEventListener("click", presses);
+buttonThe.addEventListener("click", player2);
+resetter.addEventListener("click", reset);
+
+
+function presses() {
+    total1.innerText = parseInt(total1.innerText) +1;
+}
+
+function player2(){
+    total2.innerText = parseInt(total2.innerText) +1;
+}
+function reset() {
+    total1.innerText = parseInt(total1.innerText) - (parseInt(total1.innerText));
+    total2.innerText = parseInt(total2.innerText) - (parseInt(total2.innerText));
+}
